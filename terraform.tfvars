185534af6c0c9cfbb52b8d56b2474b28ee7e5598
Sagar Soin <sasoin@navisite.com>
2024-08-10 15:13:54 -0400

    Adding profilesas

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+aws_access_key = "AKIA33JINDVNLVFH3VGM"
+aws_secret_key = "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"
--- variable.tf
+++ variable.tf
@@ -1,8 +1,8 @@
 variable "aws_access_key" {
   type = string
-  default = "AKIA33JINDVNLVFH3VGM"
+  
 }
 variable "aws_secret_key" {
   type = string
-  default = "GPAYVf6yGDuafeWYKlRad/sg09f3T2PNUCZsFQpD"
+  
 }
